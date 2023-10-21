f61b35f6ad903bf59ffa8bc610c52cf055d09298
500errocode <124433102+500errocode@users.noreply.github.com>
2023-10-21 18:47:33 +0900

    Runner - Simulations
    - Created simple classes ofr runner simulations.

---
+++ First Project/Assets/02. Script/Goalstopper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GoalStopper : MonoBehaviour
+{
+    private int _grade;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Runner runner))
+        {
+            runner.Finish (_grade++);
+        }
+    }
+}
